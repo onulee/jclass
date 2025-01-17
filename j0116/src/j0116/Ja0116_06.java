4bbd30be43a9c806934fe13e996a8fed95e28bbe
sm <onulee@naver.com>
2025-01-17 18:29:27 +0900

    배열수업

---
+++ j0116/src/j0116/Ja0116_06.java
@@ -0,0 +1,20 @@
+package j0116;
+
+public class Ja0116_06 {
+
+	public static void main(String[] args) {
+		//1. 변수선언
+		//2. 화면구현
+		//3. 번호선택 switch
+		//4. 학생성적입력
+		//5. 학생성적출력
+		//6. 수정
+		//7. 등수처리
+		//8. 종료
+		
+		
+		
+
+	}
+
+}
